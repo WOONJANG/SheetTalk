337263d32310f05c53d21b2ead448fd82c9a10b3
WOONJANG <104566843+WOONJANG@users.noreply.github.com>
2026-03-17 19:41:07 +0900

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 window.CHAT_CONFIG = {
-  API_BASE_URL: 'PUT_YOUR_APPS_SCRIPT_WEBAPP_URL_HERE'
+  API_BASE_URL: 'https://script.google.com/macros/s/AKfycbzrxAsTOFkPWRRxRpLXqyPT9mN3MmrObkf9t-NUfN3smSn4RovdHWdrT3u3BWxGuPgR/exec'
 };
